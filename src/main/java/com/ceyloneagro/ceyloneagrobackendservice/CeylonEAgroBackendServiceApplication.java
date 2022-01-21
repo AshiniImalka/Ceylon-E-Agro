31fa6a5ce9277a9f42bfd07474f5447363607bab
Imalka L.A <it18205084@my.sliit.lk>
2022-01-21 09:36:41 -0800

    initial commit

---
+++ src/main/java/com/ceyloneagro/ceyloneagrobackendservice/CeylonEAgroBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ceyloneagro.ceyloneagrobackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeylonEAgroBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeylonEAgroBackendServiceApplication.class, args);
+	}
+
+}
