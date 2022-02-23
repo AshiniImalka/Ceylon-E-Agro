9e30655bf6820695f853bc44a4674a05a474dba0
Imalka L.A <it18205084@my.sliit.lk>
2022-02-23 09:48:16 -0800

    desease identify

---
+++ src/main/java/com/ceyloneagro/ceyloneagrobackendservice/controller/DiseaseIdentifyController.java
@@ -0,0 +1,16 @@
+package com.ceyloneagro.ceyloneagrobackendservice.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(value = "/api/v1/")
+
+
+public class DiseaseIdentifyController {
+
+    
+}
